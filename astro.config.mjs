149d6cab42f496d6b8a6965b7946fbe9a81f1091
HansKristofi <kristofihans@gmail.com>
2026-03-16 20:36:40 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://HansKristofi.github.io', 
+  base: '/fotorapid2',
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()]
